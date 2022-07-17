6686cb1ee4bf813b9e2c7a84277bb227cab383d8
sumit795 <sumit795@outlook.com>
2022-07-18 00:05:03 +0530

    Added

--- src/main/java/LaunchBrowser.java
+++ src/main/java/LaunchBrowser.java
@@ -1,5 +1,16 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LaunchBrowser {
     public static void main(String[] args) {
         System.out.println("Launching web browser...");
+        System.setProperty("webdriver.chrome.driver", "/Users/sumit.kumar/Documents/Sumit/selenium_drivers/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/");
+        String getTitle = driver.getTitle();
+        System.out.println(getTitle);
+        String getURL = driver.getCurrentUrl();
+        System.out.println(getURL);
+
     }
 }
